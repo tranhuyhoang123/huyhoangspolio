7a9adc08d3f3d53af29084cb98f1024c57205fb0
tranhuyhoang123 <165565575+tranhuyhoang123@users.noreply.github.com>
2024-04-10 10:27:09 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// Javascrip Document
+var typed = new Typed('#element', {
+    strings: ["Web Designer,UX/UI Designer.", "Code Developer.","Apps Designer."],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true,
+    });
+    
+    new WOW().init();
+    
